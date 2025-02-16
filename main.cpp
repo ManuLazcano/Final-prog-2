8deaa191efda2983199ae8fe8367ce60d6de0c85
ManuLazcano <manu.lazcano17@gmail.com>
2025-02-16 11:24:12 -0300

    Setup the game loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <raylib.h>
+#include <string>
 
 int main()
 {
+    const int gameWidth = 300;
+    const int gameHeight = 600;
+    const std::string gameName = "Sandtrix";
+    Color darkBlue = { 44, 44, 127, 255};
 
+    InitWindow(gameWidth, gameHeight, gameName.c_str());
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(darkBlue);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
