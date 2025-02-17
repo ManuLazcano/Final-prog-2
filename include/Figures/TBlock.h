e279ad4ec38ccb6ce3825d3d42b58a7d7d8fb58b
ManuLazcano <manu.lazcano17@gmail.com>
2025-02-16 21:27:31 -0300

    feat: create TBlock class

---
+++ include/Figures/TBlock.h
@@ -0,0 +1,13 @@
+#ifndef TBLOCK_H
+#define TBLOCK_H
+
+#include "Block.h"
+#include "Position.h"
+
+class TBlock : public Block
+{
+    public:
+        TBlock();
+};
+
+#endif // TBLOCK_H
---
+++ src/Figures/TBlock.cpp
@@ -0,0 +1,10 @@
+#include "TBlock.h"
+
+TBlock::TBlock()
+{
+    id = 6;
+    cells[0] = {Position(0,1), Position(1,0), Position(1,1), Position(1,2)};
+    cells[1] = {Position(0,1), Position(1,1), Position(1,2), Position(2,1)};
+    cells[2] = {Position(1,0), Position(1,1), Position(1,2), Position(2,1)};
+    cells[3] = {Position(0,1), Position(1,0), Position(1,1), Position(2,1)};
+}
