757aed0553e3ac555418d8ec459f326a0c443599
ManuLazcano <manu.lazcano17@gmail.com>
2025-02-17 01:28:49 -0300

    feat: add logic to move blocks down automatically

--- main.cpp
+++ main.cpp
@@ -3,19 +3,37 @@
 
 #include "Game.h"
 
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval) {
+    double currentTime = GetTime();
+
+    if(currentTime - lastUpdateTime >= interval) {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
     const int gameWidth = 300;
     const int gameHeight = 600;
     const std::string gameName = "Sandtrix";
     Color darkBlue = { 44, 44, 127, 255};
+
     Game game = Game();
+    const double milliseconds = 0.2;
 
     InitWindow(gameWidth, gameHeight, gameName.c_str());
     SetTargetFPS(60);
 
     while(!WindowShouldClose()) {
         game.handleInput();
+
+        if(eventTriggered(milliseconds)) {
+            game.moveBlockDown();
+        }
         BeginDrawing();
         ClearBackground(darkBlue);
         game.draw();
