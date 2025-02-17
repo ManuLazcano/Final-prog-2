cada5679203c4e9e769c75d3a9e591f11770a110
ManuLazcano <manu.lazcano17@gmail.com>
2025-02-16 21:20:31 -0300

    feat: create SBlock class

---
+++ include/Figures/SBlock.h
@@ -0,0 +1,13 @@
+#ifndef SBLOCK_H
+#define SBLOCK_H
+
+#include "Block.h"
+#include "Position.h"
+
+class SBlock : public Block
+{
+    public:
+        SBlock();
+};
+
+#endif // SBLOCK_H
---
+++ src/Figures/SBlock.cpp
@@ -0,0 +1,10 @@
+#include "SBlock.h"
+
+SBlock::SBlock()
+{
+    id = 5;
+    cells[0] = {Position(0,1), Position(0,2), Position(1,0), Position(1,1)};
+    cells[1] = {Position(0,1), Position(1,1), Position(1,2), Position(2,2)};
+    cells[2] = {Position(1,1), Position(1,2), Position(2,0), Position(2,1)};
+    cells[3] = {Position(0,0), Position(1,0), Position(1,1), Position(2,1)};
+}
