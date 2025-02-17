d243b2f2f3df307fa506948c5cb8174883eb02b4
ManuLazcano <manu.lazcano17@gmail.com>
2025-02-17 11:45:05 -0300

    feat: create UI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "Game.h"
+#include "colors.h"
 
 double lastUpdateTime = 0;
 
@@ -20,23 +21,43 @@ int main()
     const int gameWidth = 500;
     const int gameHeight = 620;
     const std::string gameName = "Sandtrix";
-    Color darkBlue = { 44, 44, 127, 255};
-
-    Game game = Game();
-    const double milliseconds = 0.2;
 
     InitWindow(gameWidth, gameHeight, gameName.c_str());
     SetTargetFPS(60);
 
+    Game game = Game();
+    const double milliseconds = 0.2;
+    // Variables relacionadas con la UI
+    Font defaultFont = GetFontDefault();
+    double fontSize = 24;
+    double spacing = 2;
+    Vector2 positionScoreText = {365, 15};
+    Vector2 positionNextText = {370, 175};
+    Vector2 positionGameOverText = {330, 450};
+    Rectangle positionRectangleScore = {320, 55, 170, 60};
+    Rectangle positionRectangleNextBlock = {320, 215, 170, 180};
+    double roundness = 0.3;
+    double segments = 6;
+
     while(!WindowShouldClose()) {
         game.handleInput();
-
         if(eventTriggered(milliseconds)) {
             game.moveBlockDown();
         }
+
         BeginDrawing();
+
         ClearBackground(darkBlue);
+        DrawTextEx(defaultFont, "Score", positionScoreText, fontSize, spacing, WHITE);
+        DrawTextEx(defaultFont, "Next", positionNextText, fontSize, spacing, WHITE);
+        if(game.gameOver) {
+            DrawTextEx(defaultFont, "GAME OVER", positionGameOverText, fontSize, spacing, WHITE);
+        }
+        DrawRectangleRounded(positionRectangleScore, roundness, segments, lightBlue);
+        DrawRectangleRounded(positionRectangleNextBlock, roundness, segments, lightBlue);
+
         game.draw();
+
         EndDrawing();
     }
 
